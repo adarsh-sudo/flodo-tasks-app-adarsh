02f2bfc2a7ad3e46a7e158a7be24fa63ed4a9680
adarsh-sudo <writetoadarshsoni@gmail.com>
2026-03-28 13:37:12 +0530

    Add 'id' to list_display in TaskAdmin for improved task identification

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,6 +3,6 @@ from .models import Task
 # Register your models here.
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'due_date', 'blocked_by')
+    list_display = ('id', 'title', 'status', 'due_date', 'blocked_by')
     list_filter = ('status',)
     search_fields = ('title', 'description')
